46839a7d351911e08bf6cbf927e285a14afb1f51
Влад <zarya.vlad@yandex.ru>
2023-12-21 14:36:32 +0300

    run sim

---
+++ PulseApp-vlad/Screens/Diet/DietViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DietViewController.swift
+//  PulseApp-vlad
+//
+//  Created by Влад on 21.12.23.
+//
+
+import UIKit
+
+class DietViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PulseApp-vlad/Screens/HeartRate/Analyzing/AnalyzViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyzViewController.swift
+//  PulseApp-vlad
+//
+//  Created by Влад on 21.12.23.
+//
+
+import Foundation
